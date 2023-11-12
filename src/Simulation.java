7a1106f3e6e0e9a3f2df51a8bd5b10c095273572
AntonioLuizVeloso <92433849+AntonioLuizVeloso@users.noreply.github.com>
2023-11-12 12:02:27 +0800

    first commit

---
+++ src/Simulation.java
@@ -0,0 +1,19 @@
+/* S12 Group 3: Full Associative Mapping + Most Recently Used
+ * IBAOC, Christian Gabriel P.
+ * MAGDAMO, Bien Rafael O.
+ * VALERO, Nigel Kristoffer C.
+ * VELOSO, Antonio Luiz G.
+ */
+
+public class Simulation {
+    public static int cacheBlocks = 32;
+    public static int cacheLine = 64;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(cacheBlocks);
+    }
+
+    public static void mapping(int[] memSeq) {
+        
+    }
+}
